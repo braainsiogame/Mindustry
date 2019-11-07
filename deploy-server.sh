5eb85587578dc7a4ea8383b93bf148e9775e8345
Patrick 'Quezler' Mounier <patrick@queztech.net>
2019-11-07 08:58:50 +0100

    Fix 〃

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,4 +1,4 @@
-version=$(git describe --tags --abbrev=0 | tr --delete v)
+version=$(git describe --tags --abbrev=0 | sed 's/^.//')
 
 ./gradlew server:dist -Pbuildversion=$version
 scp server/build/libs/server-release.jar root@mindustry.nydus.app:/root/mindustry/server/
