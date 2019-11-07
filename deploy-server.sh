0db3ae1d22dc0d7b2831cecfeadd85b1286a338b
Patrick 'Quezler' Mounier <patrick@queztech.net>
2019-11-07 08:55:36 +0100

    Compile using latest tag

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,2 +1,4 @@
-./gradlew server:dist
+version=$(git describe --tags --abbrev=0 | tr --delete v)
+
+./gradlew server:dist -Pbuildversion=$version
 scp server/build/libs/server-release.jar root@mindustry.nydus.app:/root/mindustry/server/
