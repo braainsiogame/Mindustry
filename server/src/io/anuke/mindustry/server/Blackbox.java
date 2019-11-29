444f5248e81dbf0e2c977c4c8bb8e609f53b02f5
Patrick 'Quezler' Mounier <patrick@queztech.net>
2019-11-29 16:50:48 +0100

    Rollback seems to work

--- server/src/io/anuke/mindustry/server/Blackbox.java
+++ server/src/io/anuke/mindustry/server/Blackbox.java
@@ -1,9 +1,13 @@
 package io.anuke.mindustry.server;
 
 import io.anuke.arc.*;
+import io.anuke.arc.collection.*;
+import io.anuke.arc.collection.Array.*;
 import io.anuke.arc.files.*;
 import io.anuke.arc.util.*;
 import io.anuke.mindustry.core.GameState.*;
+import io.anuke.mindustry.entities.type.*;
+import io.anuke.mindustry.gen.*;
 import io.anuke.mindustry.io.*;
 
 import java.io.*;
@@ -21,25 +25,35 @@ public class Blackbox implements ApplicationListener{
         Timer.schedule(() -> {
             if (!state.is(State.playing)) return;
 
-            FileHandle file = saveDirectory.child(filenameFor(timestamp));
-            
-            Core.app.post(() -> {
-                SaveIO.save(file);
-                info("Autosaved {0}.", file);
-            });
-        }, 5f, 5f);
+            Array<Player> players = new Array<>();
+            for(Player p : playerGroup.all()){
+                players.add(p);
+                p.setDead(true);
+            }
+
+            logic.reset();
+            SaveIO.load(saveDirectory.child(filenameFor(mostRecent())));
+            Call.onWorldDataBegin();
+            logic.play();
+
+            for(Player p : players){
+                if(p.con == null) continue;
+
+                p.reset();
+                netServer.sendWorldData(p);
+            }
+
+//            FileHandle file = saveDirectory.child(filenameFor(timestamp));
+//
+//            Core.app.post(() -> {
+//                SaveIO.save(file);
+//                info("Autosaved {0}.", file);
+//            });
+        }, 5f, 10f);
 
         //
 
-        long mostRecent = 0;
-        for(FileHandle file : saveDirectory.list()){
-            if(file.extension().equals(saveExtension) && file.name().startsWith(prefix)){
-                long ts = Long.parseLong(file.nameWithoutExtension().replace(prefix, ""));
-                if (ts > mostRecent){
-                    mostRecent = ts;
-                }
-            }
-        }
+        long mostRecent = mostRecent();
         Log.info("most recent: " + mostRecent);
 
         SaveIO.load(saveDirectory.child(filenameFor(mostRecent)));
@@ -60,5 +74,19 @@ public class Blackbox implements ApplicationListener{
 
     protected String filenameFor(long timestamp){
         return prefix + timestamp + "." + saveExtension;
+    }
+
+    protected long mostRecent(){
+        long mostRecent = 0;
+        for(FileHandle file : saveDirectory.list()){
+            if(file.extension().equals(saveExtension) && file.name().startsWith(prefix)){
+                long ts = Long.parseLong(file.nameWithoutExtension().replace(prefix, ""));
+                if (ts > mostRecent){
+                    mostRecent = ts;
+                }
+            }
+        }
+        Log.info("most recent: " + mostRecent);
+        return mostRecent;
     }
 }
