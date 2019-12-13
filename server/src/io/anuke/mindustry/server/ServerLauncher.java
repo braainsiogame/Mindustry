658bc435af9e8039e396a69c9ec7e566262fc32e
Patrick 'Quezler' Mounier <patrick@queztech.net>
2019-12-13 10:45:28 +0100

    Inject blackbox and afk back in

--- server/src/io/anuke/mindustry/server/ServerLauncher.java
+++ server/src/io/anuke/mindustry/server/ServerLauncher.java
@@ -7,6 +7,7 @@ import io.anuke.arc.files.*;
 import io.anuke.arc.util.*;
 import io.anuke.mindustry.*;
 import io.anuke.mindustry.core.*;
+import io.anuke.mindustry.entities.type.*;
 import io.anuke.mindustry.game.EventType.*;
 import io.anuke.mindustry.mod.*;
 import io.anuke.mindustry.net.Net;
@@ -62,6 +63,18 @@ public class ServerLauncher implements ApplicationListener{
         Core.app.addListener(logic = new Logic());
         Core.app.addListener(netServer = new NetServer());
         Core.app.addListener(new ServerControl(args));
+        Core.app.addListener(new Blackbox());
+
+        Timer.schedule(() -> {
+            for(Player p : playerGroup.all()){
+                if(p.velocity().isZero(0.01f)){
+                    p.afkSeconds++;
+                }else{
+                    p.afkSeconds=0;
+                }
+            }
+
+        }, 0f, 1f);
 
         mods.each(Mod::init);
 
