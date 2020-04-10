fc3016cdf218b6e0bc74a53bcd3990eca201c9ac
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-04-10 10:43:07 +0200

    Make reactors damage tiles

--- core/src/mindustry/world/blocks/power/NuclearReactor.java
+++ core/src/mindustry/world/blocks/power/NuclearReactor.java
@@ -135,6 +135,8 @@ public class NuclearReactor extends PowerGenerator{
             Time.run(Mathf.random(40), () -> Effects.effect(Fx.nuclearcloud, tile.worldx(), tile.worldy()));
         }
 
+        Damage.damage(tile.worldx(), tile.worldy(), explosionRadius * tilesize, explosionDamage * 4);
+
         for(int i = 0; i < 20; i++){
             Time.run(Mathf.random(50), () -> {
                 tr.rnd(Mathf.random(40f));
