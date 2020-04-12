3af1bff230bb8cb54e2de0b27403b549bdbf98d2
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-04-12 09:43:36 +0200

    Fix around tile calculation

--- core/src/mindustry/world/Block.java
+++ core/src/mindustry/world/Block.java
@@ -950,7 +950,7 @@ public class Block extends BlockStorage{
 
     public int getAroundCount(Tile tile, Boolf<Tile> pred){
         final int[] size = {0};
-        tile.getAroundTiles(tempTiles).select(pred).each(t -> size[0] += t.block.size);
+        tile.getAroundTiles(tempTiles).select(pred).each(t -> size[0] += (t.block.size * t.block.size));
         return size[0];
     }
 }
