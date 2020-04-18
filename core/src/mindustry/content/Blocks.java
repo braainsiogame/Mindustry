0a012671512472f8e692e21d98fb37915a41cd07
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-04-18 18:06:36 +0200

    Downgrade small solar panels with not enough room

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -1235,7 +1235,7 @@ public class Blocks implements ContentList{
             powerProduction = 0.06f;
 
             flags = EnumSet.of(BlockFlag.upgradable, BlockFlag.solar);
-            upgrade = t -> t.getAroundTiles(new Array<>()).count(i -> i.block != air) == 0 ? largeSolarPanel : null;
+            upgrade = t -> t.getAroundTiles(new Array<>()).count(i -> i.block != air) == 0 ? largeSolarPanel : air;
         }};
 
         largeSolarPanel = new SolarGenerator("solar-panel-large"){{
