84645edf2913b14b870b352883355395adedea25
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-05-02 10:54:45 +0200

    Add secret core shard recipe

--- core/src/mindustry/world/blocks/storage/Vault.java
+++ core/src/mindustry/world/blocks/storage/Vault.java
@@ -1,5 +1,10 @@
 package mindustry.world.blocks.storage;
 
+import arc.func.*;
+import mindustry.content.*;
+import mindustry.plugin.spidersilk.SpiderSilk.*;
+import mindustry.world.*;
+
 public class Vault extends StorageBlock{
 
     public Vault(String name){
@@ -9,4 +14,12 @@ public class Vault extends StorageBlock{
         destructible = true;
     }
 
+    @Override
+    public void silk(Tile tile, Cons<Silk> cons){
+        if(tile.block == Blocks.vault && getAroundCount(tile, t -> t.block == Blocks.solarPanel) >= size * size + 4) cons.get(new Silk(tile){{
+            requirements = Blocks.vault.requirements;
+            trigger = () -> construct(Blocks.coreShard);
+            size = 5;
+        }});
+    }
 }
