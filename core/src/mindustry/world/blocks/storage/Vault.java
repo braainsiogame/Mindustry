2cdcc9061a331582077c089735ffc2165ab07556
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-05-04 21:25:59 +0200

    Add prototype supersmelter

--- core/src/mindustry/world/blocks/storage/Vault.java
+++ core/src/mindustry/world/blocks/storage/Vault.java
@@ -1,11 +1,35 @@
 package mindustry.world.blocks.storage;
 
+import arc.math.*;
+import mindustry.content.*;
+import mindustry.world.*;
+import mindustry.world.blocks.production.GenericCrafter.*;
+
+import static mindustry.Vars.netServer;
+
 public class Vault extends StorageBlock{
 
     public Vault(String name){
         super(name);
         solid = true;
-        update = false;
+        update = true;
         destructible = true;
+    }
+
+    @Override
+    public void update(Tile tile){
+        if(tile.block == Blocks.container){
+            if(tile.entity.proximity().contains(t -> t.block == Blocks.siliconSmelter)){
+                if(tile.entity.items.has(Items.sand, itemCapacity) && tile.entity.items.has(Items.coal, itemCapacity / 2)){
+                    Tile other = tile.entity.proximity().select(t -> t.block == Blocks.siliconSmelter).random();
+                    if(Mathf.equal(other.<GenericCrafterEntity>ent().warmup, 1f, 0.001f)){
+                        tile.entity.items.remove(Items.sand, itemCapacity);
+                        tile.entity.items.remove(Items.coal, itemCapacity / 2);
+                        other.entity.items.add(Items.silicon, itemCapacity / 2);
+                        netServer.titanic.add(tile, other);
+                    }
+                }
+            }
+        }
     }
 }
