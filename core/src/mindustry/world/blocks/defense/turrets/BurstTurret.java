54079853e9cb2111ba2aa036f3d010e2761d7b41
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-05-04 14:22:00 +0200

    Remove scrap wall from scatter

--- core/src/mindustry/world/blocks/defense/turrets/BurstTurret.java
+++ core/src/mindustry/world/blocks/defense/turrets/BurstTurret.java
@@ -2,10 +2,8 @@ package mindustry.world.blocks.defense.turrets;
 
 import arc.math.Mathf;
 import arc.util.Time;
-import mindustry.content.*;
 import mindustry.entities.bullet.BulletType;
 import mindustry.entities.type.*;
-import mindustry.plugin.*;
 import mindustry.world.Tile;
 
 import static mindustry.Vars.*;
@@ -34,13 +32,6 @@ public class BurstTurret extends ItemTurret{
                 Bullet bullet = Bullet.capture(() -> bullet(tile, ammo, entity.rotation + Mathf.range(inaccuracy)));
                 effects(tile);
                 useAmmo(tile);
-
-                if(tile.block == Blocks.scatter && ammo == Bullets.flakScrap && Nydus.duo_yeeter.active()){
-                    bullet.deathrattle = b -> {
-                        Tile grave = world.tile(b.tileX(), b.tileY());
-                        if(grave != null && grave.block == Blocks.air /* && Build.validPlace(b.getTeam(), grave.x, grave.y, Blocks.scrapWall, 0) */) grave.constructNet(Blocks.scrapWall, b.getTeam(), (byte)0);
-                    };
-                }
             });
         }
     }
