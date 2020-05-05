338d442b47c6d975acbea9d1ebfb74ed2f71ec2c
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-05-05 08:25:53 +0200

    Change silicon vault factory

--- core/src/mindustry/world/blocks/storage/Vault.java
+++ core/src/mindustry/world/blocks/storage/Vault.java
@@ -1,13 +1,14 @@
 package mindustry.world.blocks.storage;
 
-import arc.math.*;
+import arc.struct.*;
 import mindustry.content.*;
 import mindustry.world.*;
-import mindustry.world.blocks.production.GenericCrafter.*;
 
 import static mindustry.Vars.netServer;
 
 public class Vault extends StorageBlock{
+
+    private Array<Tile> batteries = new Array<>();
 
     public Vault(String name){
         super(name);
@@ -18,17 +19,22 @@ public class Vault extends StorageBlock{
 
     @Override
     public void update(Tile tile){
-        if(tile.block == Blocks.container){
-            if(tile.entity.proximity().contains(t -> t.block == Blocks.siliconSmelter)){
-                if(tile.entity.items.has(Items.sand, itemCapacity) && tile.entity.items.has(Items.coal, itemCapacity / 2)){
-                    Tile other = tile.entity.proximity().select(t -> t.block == Blocks.siliconSmelter).random();
-                    if(Mathf.equal(other.<GenericCrafterEntity>ent().warmup, 1f, 0.001f)){
-                        tile.entity.items.remove(Items.sand, itemCapacity);
-                        tile.entity.items.remove(Items.coal, itemCapacity / 2);
-                        other.entity.items.add(Items.silicon, itemCapacity / 2);
-                        netServer.titanic.add(tile, other);
-                    }
-                }
+
+        if(tile.block != Blocks.container) return;
+
+        batteries.clear();
+        batteries = tile.entity.proximity().select(t -> t.block == Blocks.battery && t.entity.power.status == 1f);
+        if(batteries.isEmpty()) return;
+        Tile battery = batteries.random();
+
+
+        if(tile.entity.proximity().contains(t -> t.block == Blocks.siliconSmelter && (t.x == tile.x || t.y == tile.y))){
+            if(tile.entity.items.has(Items.sand, itemCapacity) && tile.entity.items.has(Items.coal, itemCapacity / 2) && tile.entity.items.get(Items.silicon) <= itemCapacity / 2){
+                tile.entity.items.remove(Items.sand, itemCapacity);
+                tile.entity.items.remove(Items.coal, itemCapacity / 2);
+                tile.entity.items.add(Items.silicon, itemCapacity / 2);
+                battery.entity.power.status = 0f;
+                netServer.titanic.add(tile, battery);
             }
         }
     }
